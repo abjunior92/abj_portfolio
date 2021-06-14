15d21be0a64ca70c9e06acb307c36a8d6a2020e5
Andrea Junior Berselli <ajberselli@coders51.com>
2021-06-14 14:55:11 +0200

    fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     siteUrl: `https://abjportfolio.netlify.app/`,
     siteDescription: `My Personal Portfolio`,
     siteLanguage: `en`,
-    siteImage: `src/images/presentation.png`,
+    siteImage: `/src/images/presentation.png`,
     author: `Andrea Junior Berselli`
   },
   plugins: [
