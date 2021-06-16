4969059f2c36be19dd567be04b0c8f060dbea8df
Andrea Junior Berselli <ajberselli@coders51.com>
2021-06-16 15:49:19 +0200

    changes on mdx files

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import ProjectCard from "../@lekoarts/gatsby-theme-cara/components/project-card";
 import ContactCard from "../@lekoarts/gatsby-theme-cara/components/contact-card";
+import SectionText from "../@lekoarts/gatsby-theme-cara/components/section-text";
+import HeaderText from "../@lekoarts/gatsby-theme-cara/components/header-text";
 
 const components = {
   // eslint-disable-next-line react/display-name
@@ -10,11 +12,13 @@ const components = {
     </ProjectCard>
   ),
   // eslint-disable-next-line react/display-name
-  ContactCard: ({ icon, link, title, bg, children, ...props }) => (
-    <ContactCard {...props} icon={icon} link={link} title={title} bg={bg}>
-      {children}
-    </ContactCard>
-  )
+  ContactCard: ({ icon, link, title, ...props }) => (
+    <ContactCard {...props} icon={icon} link={link} title={title} />
+  ),
+  // eslint-disable-next-line react/display-name
+  SectionText: ({ text, ...props }) => <SectionText {...props} text={text} />,
+  // eslint-disable-next-line react/display-name
+  HeaderText: ({ text, ...props }) => <HeaderText {...props} text={text} />
 };
 
 export default components;
